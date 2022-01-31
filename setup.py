os-climate/physrisk

commit 05d60a5842cbce953a477573ba95908a71ac437d

Derive install requirements from requirements.txt
Previously, the install_requires setup parameter maintained its own list
of version-locked packages. Now the install requirements are taken from
the existing requirements.txt file.

Additionally, the requirements.txt file is now generated with
--generate-hashes for added assurance.

Signed-off-by: Nathan Gillett <ngillett@redhat.com>

setup.py
@@ -6,4 +6,17 @@ def get_version():
     with codecs.open(os.path.join(dir, "VERSION"), 'r') as fp:
         return fp.read()
 
-setup(version = get_version())
+
+def get_requirements():
+    with open("requirements.in") as reqs:
+        return [
+            line.split()[0]
+            for line in reqs.read().splitlines()
+            if not line.startswith("#")
+        ]
+
+
+setup(
+    version = get_version(),
+    install_requires=get_requirements(),
+)

setup.cfg
@@ -18,14 +18,6 @@ packages = find:
 package_dir =
     =src
 python_requires = >=3.6
-install_requires =
-    affine==2.3.0
-    numpy==1.22.0
-    pydantic==1.9.0
-    python-dotenv==0.19.2
-    requests==2.27.1
-    s3fs==2022.1.0
-    zarr==2.10.3
 
 [options.packages.find]
 where = src

commit 9b6e22f60d23e3fa14b6cb1b83cbb6d25d285ab0

Fix: setup.py's get_requirements designed for wrong file
Commit 05d60a5842cbce953a477573ba95908a71ac437d introduced a function in
setup.py to derive install requirements from requirements.txt when it
should've been intended for requirements.in. This commit corrects the
mistake.

Signed-off-by: Nathan Gillett <ngillett@redhat.com>

setup.py
@@ -8,12 +8,8 @@ def get_version():
 
 
 def get_requirements():
-    with open("requirements.in") as reqs:
-        return [
-            line.split()[0]
-            for line in reqs.read().splitlines()
-            if not line.startswith("#")
-        ]
+    with open("requirements.in") as f:
+        return f.read().splitlines()
 
 
 setup(
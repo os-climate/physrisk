b83770bf6d2534e3e467c48ccab52663f22ce798
Nathan Gillett <ngillett@redhat.com>
2022-02-09 12:33:49 -0500

    Clean-up leftover requirements artifacts
    Removes get_requirements from setup.py and requirements.in import in
    MANIFEST.in
    
    Signed-off-by: Nathan Gillett <ngillett@redhat.com>

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-import codecs
 import os
 
 from setuptools import setup
@@ -6,13 +5,8 @@ from setuptools import setup
 
 def get_version():
     dir = os.path.abspath(os.path.dirname(__file__))
-    with codecs.open(os.path.join(dir, "VERSION"), "r") as fp:
+    with open(os.path.join(dir, "VERSION"), "r") as fp:
         return fp.read()
-
-
-def get_requirements():
-    with open("requirements.in") as f:
-        return f.read().splitlines()
 
 
 setup(version=get_version())

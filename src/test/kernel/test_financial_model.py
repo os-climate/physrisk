7507b6fac5f240b80bb49426972e2536b817311f
Joe Moorhouse <joe.moorhouse@gmail.com>
2022-03-17 22:27:04 +0000

    Redesign of financial model

--- src/test/kernel/test_financial_model.py
+++ src/test/kernel/test_financial_model.py
@@ -7,6 +7,7 @@ from physrisk.data.pregenerated_hazard_model import ZarrHazardModel
 from physrisk.kernel import calculation
 from physrisk.kernel.assets import Asset, PowerGeneratingAsset
 from physrisk.kernel.financial_model import FinancialDataProvider, FinancialModel
+from physrisk.kernel.loss_model import LossModel
 
 from ..data.hazard_model_store import TestData, get_mock_hazard_model_store
 
@@ -29,16 +30,17 @@ class TestAssetImpact(unittest.TestCase):
         store = get_mock_hazard_model_store(TestData.longitudes, TestData.latitudes, curve)
         hazard_model = ZarrHazardModel(source_paths=calculation.get_default_zarr_source_paths(), store=store)
 
-        model = FinancialModel(hazard_model=hazard_model)
+        model = LossModel(hazard_model=hazard_model)
 
         data_provider = MockFinancialDataProvider()
+        financial_model = FinancialModel(data_provider)
 
         assets = [PowerGeneratingAsset(lat, lon) for lon, lat in zip(TestData.longitudes, TestData.latitudes)]
         measures = model.get_financial_impacts(
-            assets, data_provider=data_provider, scenario="rcp8p5", year=2050, sims=100000
+            assets, financial_model=financial_model, scenario="rcp8p5", year=2050, sims=100000
         )
 
         np.testing.assert_array_almost_equal_nulp(
             measures["RiverineInundation"]["percentile_values"],
-            np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 365000.0, 365000.0, 365000.0, 730000.0]),
+            np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1000.0, 1000.0, 1000.0, 2000.0]),
         )

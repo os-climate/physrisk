45802770376b0e2c53844efb8e94b5e8ba1a3fbf
Joe Moorhouse <joe.moorhouse@gmail.com>
2022-07-04 17:25:46 +0100

    Update hazard onboarding scripts
    Signed-off-by: Joe Moorhouse <joe.moorhouse@gmail.com>

--- notebooks/hazard_onboarding/mapbox_utilities.py
+++ notebooks/hazard_onboarding/mapbox_utilities.py
@@ -1,43 +1,85 @@
-import json
-import logging
-import os
-import sys
+import base64, hashlib, json, logging, os, sys
 from time import sleep
+from os import listdir
+from os.path import isfile, join
+from typing import List, Tuple
 
-import numpy as np
-import rasterio
-import seaborn as sns
 from affine import Affine
+import numpy as np
 from mapbox import Uploader
 from matplotlib import pyplot as plt
+import rasterio
 from rasterio.enums import Resampling
+import seaborn as sns
+import s3fs
+from tifffile.tifffile import TiffFile
+import zarr
 
-LOG = logging.getLogger("Mapbox onboarding")
-LOG.setLevel(logging.INFO)
+def alphanumeric(text):
+    """Return alphanumeric hash from supplied string."""
+    hash_int = int.from_bytes(hashlib.sha1(text.encode('utf-8')).digest(), 'big')
+    return base36encode(hash_int)
 
+def base36encode(number, alphabet='0123456789abcdefghijklmnopqrstuvwxyz'):
+    """Converts an integer to a base36 string."""
+    if not isinstance(number, int):
+        raise TypeError('number must be an integer')
 
-def load_resampled(input_path, target_width=None):
+    base36 = ''
 
+    if number < 0:
+        raise TypeError('number must be positive')
+
+    if 0 <= number < len(alphabet):
+        return sign + alphabet[number]
+
+    while number != 0:
+        number, i = divmod(number, len(alphabet))
+        base36 = alphabet[i] + base36
+
+    return base36
+
+def load_s3(s3_source, src_bucket, src_prefix, filename, target_width=None):
+    with s3_source.open(os.path.join(src_bucket, src_prefix, filename)) as f:
+        with rasterio.open(f) as dataset:
+            return load(dataset, target_width)
+
+def load(dataset, target_width=None):
+    scaling = 1 if target_width is None else float(target_width) / dataset.width
+    width = int(dataset.width * scaling)
+    height = int(dataset.height * scaling)
+
+    # data = src.read(1)
+    # resample data to target shape
+    data = dataset.read(1) if scaling == 1 else dataset.read(1, out_shape=(dataset.count, height, width), resampling=Resampling.bilinear)
+
+    # scale image transform
+    t = dataset.transform
+    transform = Affine(t.a / scaling, t.b / scaling, t.c, t.d / scaling, t.e / scaling, t.f)
+    transform = dataset.transform * dataset.transform.scale(
+        (float(dataset.width) / data.shape[-1]), (float(dataset.height) / data.shape[-2])
+    )
+
+    return (data, dataset.profile, width, height, transform)
+
+def load_fs(input_path, target_width=None):    
     with rasterio.open(input_path) as dataset:
-        scaling = 1 if target_width is None else float(target_width) / dataset.width
-        width = int(dataset.width * scaling)
-        height = int(dataset.height * scaling)
+        return load(dataset, target_width)
 
-        # data = src.read(1)
-        # resample data to target shape
-        data = dataset.read(1, out_shape=(dataset.count, height, width), resampling=Resampling.bilinear)
+def write_map_geotiff_s3(s3_source, src_bucket, src_prefix, filename, output_dir):
+    (data, profile, width, height, transform) = load_s3(s3_source, src_bucket, src_prefix, filename)
+    LOG.info("Loaded")
 
-        # scale image transform
-        t = dataset.transform
-        transform = Affine(t.a / scaling, t.b / scaling, t.c, t.d / scaling, t.e / scaling, t.f)
-        transform = dataset.transform * dataset.transform.scale(
-            (float(dataset.width) / data.shape[-1]), (float(dataset.height) / data.shape[-2])
-        )
+    write_map_geotiff(data, profile, width, height, transform, filename, output_dir)
 
-        return (data, dataset.profile, width, height, transform)
+def write_map_geotiff_fs(input_dir, filename, output_dir):
+    (data, profile, width, height, transform) = load_fs(os.path.join(input_dir, filename))
+    LOG.info("Loaded")
 
+    write_map_geotiff(data, profile, width, height, transform, filename, output_dir)
 
-def write_map_geotiff(input_dir, input_file, output_dir):
+def write_map_geotiff(data, profile, width, height, transform, filename, output_dir):
+
     max_intensity = 2.0  # 2 metres for inundation
 
     # we use a color map from Seaborn: looks good
@@ -60,7 +102,11 @@ def write_map_geotiff(input_dir, input_file, output_dir):
     map_for_json[0] = map_for_json[1] = (255, 255, 255, 0)  # no data and zero are transparent
     a[0] = a[1] = 0
 
-    colormap_path_out = os.path.join(output_dir, "colormap_" + input_file.split(".")[0] + ".json")
+    filename_stub = filename.split('.')[0]
+    alpha = alphanumeric(filename_stub)[0:6]
+    LOG.info(f"Hashing {filename_stub} as code: {alpha}")
+    
+    colormap_path_out = os.path.join(output_dir, "colormap_" + alpha + "_" + filename_stub + ".json")
     with open(colormap_path_out, "w") as f:
         colormap_info = json.dumps(
             {
@@ -71,9 +117,6 @@ def write_map_geotiff(input_dir, input_file, output_dir):
             }
         )
         f.writelines(colormap_info)
-
-    (data, profile, width, height, transform) = load_resampled(os.path.join(input_dir, input_file), 30000)
-    LOG.info("Resampled")
 
     mask = data < 0  # == -9999.0 is alternative
     mask2 = data > max_intensity
@@ -99,7 +142,7 @@ def write_map_geotiff(input_dir, input_file, output_dir):
     profile["height"] = height
     profile["transform"] = transform
 
-    path_out = os.path.join(output_dir, "map_" + input_file)
+    path_out = os.path.join(output_dir, "map_" + alpha + "_" + filename)
 
     with rasterio.open(path_out, "w", **profile) as dst:
         # dst.colorinterp = [ ColorInterp.red, ColorInterp.green, ColorInterp.blue ]
@@ -107,11 +150,11 @@ def write_map_geotiff(input_dir, input_file, output_dir):
         # dst.write_colormap(1, map)
         LOG.info("Writing R 1/4")
         dst.write(reds[result], 1)
-        LOG.info("Writing G 1/4")
+        LOG.info("Writing G 2/4")
         dst.write(greens[result], 2)
-        LOG.info("Writing B 1/4")
+        LOG.info("Writing B 3/4")
         dst.write(blues[result], 3)
-        LOG.info("Writing A 1/4")
+        LOG.info("Writing A 4/4")
         dst.write(a[result], 4)
     LOG.info("Complete")
     return (path_out, colormap_path_out)
@@ -145,18 +188,20 @@ def upload_geotiff(path, id):
             sleep(5)
 
 
-input_dir = r"/root/code/osc/inputs/"
-input_file = "inunriver_rcp8p5_MIROC-ESM-CHEM_2080_rp01000.tif"
-output_dir = r"/root/code/osc/inputs/"
+#LOG = logging.getLogger("Mapbox onboarding")
+#LOG.setLevel(logging.INFO)
+            
+# input_dir = r"/root/code/osc/inputs/"
+# input_file = "inunriver_rcp8p5_MIROC-ESM-CHEM_2080_rp01000.tif"
+# output_dir = r"/root/code/osc/inputs/"
 
-# print((colormap, map))
-handler = logging.StreamHandler(sys.stdout)
-handler.setLevel(logging.DEBUG)
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-handler.setFormatter(formatter)
-LOG.addHandler(handler)
+# # print((colormap, map))
+# handler = logging.StreamHandler(sys.stdout)
+# handler.setLevel(logging.DEBUG)
+# formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+# handler.setFormatter(formatter)
+# LOG.addHandler(handler)
 
-(colormap, map) = write_map_geotiff(input_dir, input_file, output_dir)
-upload_geotiff("/root/code/osc/inputs/map_inunriver_rcp8p5_MIROC-ESM-CHEM_2080_rp01000.tif", "test3")
+# (colormap, map) = write_map_geotiff(input_dir, input_file, output_dir)
+# upload_geotiff("/root/code/osc/inputs/map_inunriver_rcp8p5_MIROC-ESM-CHEM_2080_rp01000.tif", "test3")
 
-# 2 is

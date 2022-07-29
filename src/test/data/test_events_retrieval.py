dd95d4974ba0c33eaba1aa9c109dffa2bb2a62fa
Deepak Baskaran <103453423+deepakbaskaran@users.noreply.github.com>
2022-07-29 09:54:28 +0100

    Changes to zarr_reader file to incorporate max and min interpolations. (#92)
    * Changes to inundation model to calculate the maximum inundation curve on a square grid surrounding a given location.
    * Adding max and min methods to zarr_reader interpolation methods.

--- src/test/data/test_events_retrieval.py
+++ src/test/data/test_events_retrieval.py
@@ -5,7 +5,11 @@ import pathlib
 import shutil
 import tempfile
 import unittest
-from test.data.hazard_model_store import TestData, get_mock_hazard_model_store_single_curve
+from test.data.hazard_model_store import (
+    TestData,
+    get_mock_hazard_model_store_single_curve,
+    mock_hazard_model_store_inundation,
+)
 
 import numpy as np
 import numpy.testing
@@ -55,15 +59,66 @@ class TestEventRetrieval(unittest.TestCase):
         x = np.array([3.2, 6.7, 7.9])  # column indices
         image_coords = np.stack([x, y])
         data_zarr = zarr.array(data)
-        candidate = ZarrReader._linear_interp_frac_coordinates(data_zarr, image_coords, np.array([0, 1]))
+        candidate_lin = ZarrReader._linear_interp_frac_coordinates(
+            data_zarr, image_coords, np.array([0, 1]), interpolation="linear"
+        )
+        candidate_max = ZarrReader._linear_interp_frac_coordinates(
+            data_zarr, image_coords, np.array([0, 1]), interpolation="max"
+        )
+        candidate_min = ZarrReader._linear_interp_frac_coordinates(
+            data_zarr, image_coords, np.array([0, 1]), interpolation="min"
+        )
+
+        image_coords_surr = np.stack(
+            [
+                np.concatenate([np.floor(x), np.floor(x) + 1, np.floor(x), np.floor(x) + 1]),
+                np.concatenate([np.floor(y), np.floor(y), np.floor(y) + 1, np.floor(y) + 1]),
+            ]
+        )
+        values_surr = ZarrReader._linear_interp_frac_coordinates(
+            data_zarr, image_coords_surr, np.array([0, 1]), interpolation="linear"
+        ).reshape((4, 3, 2))
         interp_scipy0 = scipy.interpolate.interp2d(np.linspace(0, 9, 10), np.linspace(0, 9, 10), data0, kind="linear")
         interp_scipy1 = scipy.interpolate.interp2d(np.linspace(0, 9, 10), np.linspace(0, 9, 10), data1, kind="linear")
-        expected0 = interp_scipy0(x, y).diagonal().reshape(len(y))
-        expected1 = interp_scipy1(x, y).diagonal().reshape(len(y))
-        numpy.testing.assert_allclose(candidate[:, 0], expected0, rtol=1e-6)
-        numpy.testing.assert_allclose(candidate[:, 1], expected1, rtol=1e-6)
+        expected0_lin = interp_scipy0(x, y).diagonal().reshape(len(y))
+        expected1_lin = interp_scipy1(x, y).diagonal().reshape(len(y))
+        expected0_max = np.max(values_surr[:, :, 0], axis=0)
+        expected1_max = np.max(values_surr[:, :, 1], axis=0)
+        expected0_min = np.min(values_surr[:, :, 0], axis=0)
+        expected1_min = np.min(values_surr[:, :, 1], axis=0)
+
+        numpy.testing.assert_allclose(candidate_lin[:, 0], expected0_lin, rtol=1e-6)
+        numpy.testing.assert_allclose(candidate_lin[:, 1], expected1_lin, rtol=1e-6)
+        numpy.testing.assert_allclose(candidate_max[:, 0], expected0_max, rtol=1e-6)
+        numpy.testing.assert_allclose(candidate_max[:, 1], expected1_max, rtol=1e-6)
+        numpy.testing.assert_allclose(candidate_min[:, 0], expected0_min, rtol=1e-6)
+        numpy.testing.assert_allclose(candidate_min[:, 1], expected1_min, rtol=1e-6)
         # array([0.43854423, 0.62290176, 0.50660137])
         # array([0.58346331, 0.72702827, 0.62629474])
+        # array([0.60981554, 0.75222318, 0.72041193])
+        # array([0.72817392, 0.82838968, 0.79717553])
+        # array([0.34773063, 0.45343876, 0.45907147])
+        # array([0.50555484, 0.59275348, 0.58789487])
+
+    def test_zarr_geomax(self):
+        lons_ = np.array([3.92783])
+        lats_ = np.array([50.882394])
+        curve = np.array(
+            [0.00, 0.06997928, 0.2679602, 0.51508933, 0.69842442, 0.88040525, 1.11911115, 1.29562478, 1.47200677]
+        )
+        set_id = r"inundation/wri/v2\\inunriver_rcp8p5_MIROC-ESM-CHEM_2080"
+        interpolation = "linear"
+        delta_km = 0.100
+        n_grid = 10
+        store_ = mock_hazard_model_store_inundation(lons_, lats_, curve)
+        zarrreader_ = ZarrReader(store_)
+        curves_max_candidate, _ = zarrreader_.get_max_curves(
+            set_id, lons_, lats_, interpolation=interpolation, delta_km=delta_km, n_grid=n_grid
+        )
+        curves_max_expected = np.array(
+            [[0.00, 0.04917953, 0.1883151, 0.3619907, 0.49083358, 0.61872474, 0.78648075, 0.91052965, 1.03448614]]
+        )
+        numpy.testing.assert_allclose(curves_max_candidate, curves_max_expected, rtol=1e-6)
 
     def test_zarr_reading(self):
 

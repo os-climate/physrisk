eb5abe80321720b92a9702667b6d275254512897
EglantineGiraud <eglantine.giraud@gmail.com>
2023-11-10 18:31:19 +0100

    Onboarding World Resources Institute's thermal power plant physical climate vulnerability factors
    Onboarding World Resources Institute's thermal power plant physical climate vulnerability factors
    
    Signed-off-by: EglantineGiraud <eglantine.giraud@gmail.com>

--- src/physrisk/kernel/impact_distrib.py
+++ src/physrisk/kernel/impact_distrib.py
@@ -38,10 +38,12 @@ class ImpactDistrib:
         return zip(self.__impact_bins[0:-1], self.__impact_bins[1:])
 
     def mean_impact(self):
+        if len(self.__impact_bins) == len(self.__prob):
+            return self.mean()
         return np.sum((self.__impact_bins[:-1] + self.__impact_bins[1:]) * self.__prob / 2)
 
     def mean(self):
-        return np.sum(self.__impact_bins * self.__prob)
+        return np.sum(self.__impact_bins * self.__prob) + self.__impact_bins[-1] * (1.0 - np.sum(self.__prob))
 
     def to_exceedance_curve(self):
         return to_exceedance_curve(self.__impact_bins, self.__prob)

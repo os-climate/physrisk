3613a40691740d4c456fb54e46f6654655a645e4
Matthew Watkins <mwatkins@linuxfoundation.org>
2023-11-23 14:35:07 +0000

    Chore: Rename tests to be compliant with PEP standards
    Signed-off-by: Matthew Watkins <mwatkins@linuxfoundation.org>

---
+++ scripts/rename-tests.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#set -x
+
+for FILE in $(find . -name "*test_*" -print | xargs echo)
+do
+	if (echo "$FILE" | grep -v ".zip" > /dev/null)
+	then
+		NEW_NAME=$(echo "$FILE" | sed "s:test_::g" | sed "s:.py:_test.py:g")
+		echo "Renaming: $FILE to $NEW_NAME"
+		git mv "$FILE" "$NEW_NAME"
+	else
+		echo "Skipping: $FILE"
+	fi
+done

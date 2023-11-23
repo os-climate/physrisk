a7c1c21ffca8276253f3402e5970e65ca436d48f
Matthew Watkins <mwatkins@linuxfoundation.org>
2023-11-23 14:36:48 +0000

    Chore: Update shell script to rename tests
    Signed-off-by: Matthew Watkins <mwatkins@linuxfoundation.org>

--- scripts/rename-tests.sh
+++ scripts/rename-tests.sh
@@ -2,14 +2,13 @@
 
 #set -x
 
-for FILE in $(find . -name "*test_*" -print | xargs echo)
-do
-	if (echo "$FILE" | grep -v ".zip" > /dev/null)
-	then
-		NEW_NAME=$(echo "$FILE" | sed "s:test_::g" | sed "s:.py:_test.py:g")
-		echo "Renaming: $FILE to $NEW_NAME"
-		git mv "$FILE" "$NEW_NAME"
-	else
-		echo "Skipping: $FILE"
-	fi
+for FILE in $(find . -name "*test_*" -print | xargs echo); do
+    if (echo "$FILE" | grep -v ".zip" > /dev/null)
+    then
+        NEW_NAME=$(echo "$FILE" | sed "s:test_::g" | sed "s:.py:_test.py:g")
+        echo "Renaming: $FILE to $NEW_NAME"
+        git mv "$FILE" "$NEW_NAME"
+    else
+        echo "Skipping: $FILE"
+    fi
 done

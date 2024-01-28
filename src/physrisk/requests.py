4cbd573c8f95137b606a46af7c736396bc3ef83b
Joe Moorhouse <5102656+joemoorhouse@users.noreply.github.com>
2024-01-28 21:48:10 +0000

    Correct ordering potential instability
    Signed-off-by: Joe Moorhouse <5102656+joemoorhouse@users.noreply.github.com>

--- src/physrisk/requests.py
+++ src/physrisk/requests.py
@@ -298,6 +298,8 @@ def _get_asset_impacts(
 
     if request.include_asset_level:
         ordered_impacts: Dict[Asset, List[AssetSingleImpact]] = {}
+        for asset in assets:
+            ordered_impacts[asset] = []
         for k, v in impacts.items():
             if request.include_calc_details:
                 if v.event is not None and v.vulnerability is not None:
@@ -322,6 +324,7 @@ def _get_asset_impacts(
 
             if isinstance(v.impact, EmptyImpactDistrib):
                 continue
+            
             impact_exceedance = v.impact.to_exceedance_curve()
             key = ImpactKey(hazard_type=k.hazard_type.__name__, scenario_id=k.scenario, year=str(k.key_year))
             hazard_impacts = AssetSingleImpact(
@@ -335,7 +338,7 @@ def _get_asset_impacts(
                 impact_std_deviation=v.impact.stddev_impact(),
                 calc_details=None if v.event is None else calc_details,
             )
-            ordered_impacts.setdefault(k.asset, []).append(hazard_impacts)
+            ordered_impacts[k.asset].append(hazard_impacts)
 
         # note that this does rely on ordering of dictionary (post 3.6)
         asset_impacts = [AssetLevelImpact(asset_id="", impacts=a) for a in ordered_impacts.values()]

9e1f2636c2f247e80179622c1a91ae8185216fb8
Floflow <floperrindb@gmail.com>
2024-06-19 16:01:27 +0100

    add definitions and label for all underlying hazards

--- src/physrisk/risk_models/generic_risk_model.py
+++ src/physrisk/risk_models/generic_risk_model.py
@@ -62,29 +62,28 @@ class GenericScoreBasedRiskMeasures(RiskMeasureCalculator):
         self.model_summary = {"Generic score based risk measure."}
         # fmt: off
         self.wind_bounds = [
-            HazardIndicatorBounds(category=Category.NODATA, hazard_type=Wind, indicator_id="max_speed", indicator_return=100, units="km/h", lower=float("-inf"), upper=63), # noqa
-            HazardIndicatorBounds(category=Category.LOW,    hazard_type=Wind, indicator_id="max_speed", indicator_return=100, units="km/h", lower=63, upper=90), # noqa
+            HazardIndicatorBounds(category=Category.LOW,    hazard_type=Wind, indicator_id="max_speed", indicator_return=100, units="km/h", lower=float("-inf"), upper=90), # noqa
             HazardIndicatorBounds(category=Category.MEDIUM, hazard_type=Wind, indicator_id="max_speed", indicator_return=100, units="km/h", lower=90, upper=119), # noqa
             HazardIndicatorBounds(category=Category.HIGH,   hazard_type=Wind, indicator_id="max_speed", indicator_return=100, units="km/h", lower=119, upper=178), # noqa
             HazardIndicatorBounds(category=Category.REDFLAG,hazard_type=Wind, indicator_id="max_speed", indicator_return=100, units="km/h", lower=178, upper=float("inf")) # noqa
         ]
         self.hail_bounds = [
-            HazardIndicatorBounds(category=Category.LOW,    hazard_type=Hail, indicator_id="days/above/5cm", indicator_return=100, units="days/year", lower=float("-inf"), upper=1), # noqa
-            HazardIndicatorBounds(category=Category.MEDIUM, hazard_type=Hail, indicator_id="days/above/5cm", indicator_return=100, units="days/year", lower=1, upper=2), # noqa
-            HazardIndicatorBounds(category=Category.HIGH,   hazard_type=Hail, indicator_id="days/above/5cm", indicator_return=100, units="days/year", lower=2, upper=3), # noqa
-            HazardIndicatorBounds(category=Category.REDFLAG,hazard_type=Hail, indicator_id="days/above/5cm", indicator_return=100, units="days/year", lower=3, upper=float("inf")) # noqa
+            HazardIndicatorBounds(category=Category.LOW,    hazard_type=Hail, indicator_id="days/above/5cm", indicator_return=1, units="days/year", lower=float("-inf"), upper=1), # noqa
+            HazardIndicatorBounds(category=Category.MEDIUM, hazard_type=Hail, indicator_id="days/above/5cm", indicator_return=1, units="days/year", lower=1, upper=2), # noqa
+            HazardIndicatorBounds(category=Category.HIGH,   hazard_type=Hail, indicator_id="days/above/5cm", indicator_return=1, units="days/year", lower=2, upper=3), # noqa
+            HazardIndicatorBounds(category=Category.REDFLAG,hazard_type=Hail, indicator_id="days/above/5cm", indicator_return=1, units="days/year", lower=3, upper=float("inf")) # noqa
         ]
         self.drought_bounds = [
-            HazardIndicatorBounds(category=Category.LOW,    hazard_type=Drought, indicator_id="months/spei3m/below/-2", indicator_return=100, units="months/year", lower=float("-inf"), upper=0.25), # noqa
-            HazardIndicatorBounds(category=Category.MEDIUM, hazard_type=Drought, indicator_id="months/spei3m/below/-2", indicator_return=100, units="months/year", lower=0.25, upper=0.5), # noqa
-            HazardIndicatorBounds(category=Category.HIGH,   hazard_type=Drought, indicator_id="months/spei3m/below/-2", indicator_return=100, units="months/year", lower=0.5, upper=1), # noqa
-            HazardIndicatorBounds(category=Category.REDFLAG,hazard_type=Drought, indicator_id="months/spei3m/below/-2", indicator_return=100, units="months/year", lower=1, upper=float("inf")) # noqa
+            HazardIndicatorBounds(category=Category.LOW,    hazard_type=Drought, indicator_id="months/spei3m/below/-2", indicator_return=1, units="months/year", lower=float("-inf"), upper=0.25), # noqa
+            HazardIndicatorBounds(category=Category.MEDIUM, hazard_type=Drought, indicator_id="months/spei3m/below/-2", indicator_return=1, units="months/year", lower=0.25, upper=0.5), # noqa
+            HazardIndicatorBounds(category=Category.HIGH,   hazard_type=Drought, indicator_id="months/spei3m/below/-2", indicator_return=1, units="months/year", lower=0.5, upper=1), # noqa
+            HazardIndicatorBounds(category=Category.REDFLAG,hazard_type=Drought, indicator_id="months/spei3m/below/-2", indicator_return=1, units="months/year", lower=1, upper=float("inf")) # noqa
         ]
         self.fire_bounds = [
-            HazardIndicatorBounds(category=Category.LOW,    hazard_type=Fire, indicator_id="fire_probability", indicator_return=100, units="% probability/month", lower=float("-inf"), upper=20), # noqa
-            HazardIndicatorBounds(category=Category.MEDIUM, hazard_type=Fire, indicator_id="fire_probability", indicator_return=100, units="% probability/month", lower=20, upper=35), # noqa
-            HazardIndicatorBounds(category=Category.HIGH,   hazard_type=Fire, indicator_id="fire_probability", indicator_return=100, units="% probability/month", lower=35, upper=50), # noqa
-            HazardIndicatorBounds(category=Category.REDFLAG,hazard_type=Fire, indicator_id="fire_probability", indicator_return=100, units="% probability/month", lower=50, upper=float("inf")) # noqa
+            HazardIndicatorBounds(category=Category.LOW,    hazard_type=Fire, indicator_id="fire_probability", indicator_return=1, units="% probability/month", lower=float("-inf"), upper=20), # noqa
+            HazardIndicatorBounds(category=Category.MEDIUM, hazard_type=Fire, indicator_id="fire_probability", indicator_return=1, units="% probability/month", lower=20, upper=35), # noqa
+            HazardIndicatorBounds(category=Category.HIGH,   hazard_type=Fire, indicator_id="fire_probability", indicator_return=1, units="% probability/month", lower=35, upper=50), # noqa
+            HazardIndicatorBounds(category=Category.REDFLAG,hazard_type=Fire, indicator_id="fire_probability", indicator_return=1, units="% probability/month", lower=50, upper=float("inf")) # noqa
         ]
         self.precipitation_bounds = [
             HazardIndicatorBounds(category=Category.LOW,    hazard_type=Precipitation, indicator_id="max/daily/water_equivalent", indicator_return=100, units="mm rainfall/day", lower=float("-inf"), upper=100), # noqa
@@ -93,10 +92,10 @@ class GenericScoreBasedRiskMeasures(RiskMeasureCalculator):
             HazardIndicatorBounds(category=Category.REDFLAG,hazard_type=Precipitation, indicator_id="max/daily/water_equivalent", indicator_return=100, units="mm rainfall/day", lower=160, upper=float("inf")) # noqa
         ]
         self.chronicHeat_bounds = [
-            HazardIndicatorBounds(category=Category.LOW,    hazard_type=ChronicHeat, indicator_id="days/above/35c", indicator_return=100, units="days/year", lower=float("-inf"), upper=10), # noqa
-            HazardIndicatorBounds(category=Category.MEDIUM, hazard_type=ChronicHeat, indicator_id="days/above/35c", indicator_return=100, units="days/year", lower=10, upper=20), # noqa
-            HazardIndicatorBounds(category=Category.HIGH,   hazard_type=ChronicHeat, indicator_id="days/above/35c", indicator_return=100, units="days/year", lower=20, upper=30), # noqa
-            HazardIndicatorBounds(category=Category.REDFLAG,hazard_type=ChronicHeat, indicator_id="days/above/35c", indicator_return=100, units="days/year", lower=30, upper=float("inf")) # noqa
+            HazardIndicatorBounds(category=Category.LOW,    hazard_type=ChronicHeat, indicator_id="days/above/35c", indicator_return=1, units="days/year", lower=float("-inf"), upper=10), # noqa
+            HazardIndicatorBounds(category=Category.MEDIUM, hazard_type=ChronicHeat, indicator_id="days/above/35c", indicator_return=1, units="days/year", lower=10, upper=20), # noqa
+            HazardIndicatorBounds(category=Category.HIGH,   hazard_type=ChronicHeat, indicator_id="days/above/35c", indicator_return=1, units="days/year", lower=20, upper=30), # noqa
+            HazardIndicatorBounds(category=Category.REDFLAG,hazard_type=ChronicHeat, indicator_id="days/above/35c", indicator_return=1, units="days/year", lower=30, upper=float("inf")) # noqa
         ]
         # fmt: on
         self._definition_lookup = {}
@@ -115,62 +114,62 @@ class GenericScoreBasedRiskMeasures(RiskMeasureCalculator):
                 RiskMeasureDefinition(
                     measure_id="measure_wind",
                     label="1-in-100 year sustained wind speed.",
-                    description="1-in-100 year sustained wind speed.",
+                    description="This wind speed model computes the maximum 1-minute sustained wind speed (in km/hr) experienced over a 100 year return period based on mean wind speed distributions from multiple Global Climate Models (GCMs).",
                 )
             ],
         )
         self._definition_lookup[Hail] = ScoreBasedRiskMeasureDefinition(
             hazard_types=[Hail.__name__],
-            values=self._definition_values(self.wind_bounds, self.wind_label_description),
+            values=self._definition_values(self.hail_bounds, self.hail_label_description),
             underlying_measures=[
                 RiskMeasureDefinition(
                     measure_id="measure_hail",
-                    label="1-in-100 year sustained wind speed.",
-                    description="1-in-100 year sustained wind speed.",
+                    label="Number of days per year where large hail (>2 inches / 5 cm in diameter) is possible.",
+                    description="This hail model computes the number of days per annum where hail exceeding 5 cm diameter is possible based on the mean distribution of several parameters across multiple bias-corrected and downscaled Global Climate Models (GCMs).",
                 )
             ],
         )
         self._definition_lookup[Drought] = ScoreBasedRiskMeasureDefinition(
             hazard_types=[Drought.__name__],
-            values=self._definition_values(self.wind_bounds, self.wind_label_description),
+            values=self._definition_values(self.drought_bounds, self.drought_label_description),
             underlying_measures=[
                 RiskMeasureDefinition(
                     measure_id="measure_drought",
-                    label="1-in-100 year sustained wind speed.",
-                    description="1-in-100 year sustained wind speed.",
+                    label="Months per year where the rolling 3-month average Standardized Precipitation Evapotranspiration Index is <2.",
+                    description="This drought model is based on the Standardized Precipitation-Evapotranspiration Index (SPEI). The SPEl is an extension of the Standardized Precipitation Index which also considers Potential Evapotranspiration (PET) in determining drought events. The SPEl is calculated from a log-logistic probability distribution function of climatic water balance (precipitation minus evapotranspiration) over a given time scale. The SPEI itself is a standardized variable with a mean value 0 and standard deviation 1. This drought model computes the number of months per annum where the 3-month rolling average\nof SPEI is below -2 based on the mean values of several parameters from bias-corrected and downscaled multiple Global Climate Models (GCMs).",
                 )
             ],
         )
         self._definition_lookup[Fire] = ScoreBasedRiskMeasureDefinition(
             hazard_types=[Fire.__name__],
-            values=self._definition_values(self.wind_bounds, self.wind_label_description),
+            values=self._definition_values(self.fire_bounds, self.fire_label_description),
             underlying_measures=[
                 RiskMeasureDefinition(
                     measure_id="measure_fire",
-                    label="1-in-100 year sustained wind speed.",
-                    description="1-in-100 year sustained wind speed.",
+                    label="The maximum value, across all months, of the monthly probability of a wildfire within 100km of the location.",
+                    description="This fire model computes the maximum monthly probability per annum of a wildfire within 100 km of a given location based on several parameters from multiple bias corrected and downscaled Global Climate Models (GCMs). For example, if the probability of occurrence of a wildfire is 5%% in July, 20%% in August, 10%% in September and 0%% for other months, the hazard indicator value is 20%.",
                 )
             ],
         )
         self._definition_lookup[Precipitation] = ScoreBasedRiskMeasureDefinition(
             hazard_types=[Precipitation.__name__],
-            values=self._definition_values(self.wind_bounds, self.wind_label_description),
+            values=self._definition_values(self.precipitation_bounds, self.precipitation_label_description),
             underlying_measures=[
                 RiskMeasureDefinition(
                     measure_id="measure_precipitation",
-                    label="1-in-100 year sustained wind speed.",
-                    description="1-in-100 year sustained wind speed.",
+                    label="1-in-100 year maximum daily total water equivalent precipitation (in mm).",
+                    description="This model computes the maximum daily water equivalent precipitation (in mm) measured at the 100 year return period based on the mean of the precipitation distribution from multiple bias corrected and downscaled Global Climate Models (GCMs).",
                 )
             ],
         )
         self._definition_lookup[ChronicHeat] = ScoreBasedRiskMeasureDefinition(
             hazard_types=[ChronicHeat.__name__],
-            values=self._definition_values(self.wind_bounds, self.wind_label_description),
+            values=self._definition_values(self.chronicHeat_bounds, self.chronicHeat_label_description),
             underlying_measures=[
                 RiskMeasureDefinition(
                     measure_id="measure_chronicHeat",
-                    label="1-in-100 year sustained wind speed.",
-                    description="1-in-100 year sustained wind speed.",
+                    label="Days per year with temperature > 35°C.",
+                    description="This heat model computes the number of days exceeding 35°C per annum based on the mean of distribution fits to the bias-corrected and downscaled high temperature distribution across multiple Global Climate Models (GCMs).",
                 )
             ],
         )
@@ -199,6 +198,31 @@ class GenericScoreBasedRiskMeasures(RiskMeasureCalculator):
     def wind_label_description(self, bounds: HazardIndicatorBounds):
         label = f"Max wind speed between {bounds.lower} and {bounds.upper} {bounds.units}"
         description = f"Max sustained wind speed between {bounds.lower} and {bounds.upper} {bounds.units}"
+        return label, description
+
+    def hail_label_description(self, bounds: HazardIndicatorBounds):
+        label = f"Max number of days per year between {bounds.lower} and {bounds.upper} {bounds.units}"
+        description = f"Max number of days per year between {bounds.lower} and {bounds.upper} {bounds.units}"
+        return label, description
+
+    def drought_label_description(self, bounds: HazardIndicatorBounds):
+        label = f"Max months per year between {bounds.lower} and {bounds.upper} {bounds.units}"
+        description = f"Max months per year between {bounds.lower} and {bounds.upper} {bounds.units}"
+        return label, description
+
+    def fire_label_description(self, bounds: HazardIndicatorBounds):
+        label = f"Max value, across all months, of the monthly probability of a wildfire between {bounds.lower} and {bounds.upper} {bounds.units}"
+        description = f"Max value, across all months, of the monthly probability of a wildfire between {bounds.lower} and {bounds.upper} {bounds.units}"
+        return label, description
+
+    def precipitation_label_description(self, bounds: HazardIndicatorBounds):
+        label = f"Max daily total water equivalent precipitation between {bounds.lower} and {bounds.upper} {bounds.units}"
+        description = f"Max daily total water equivalent precipitation between {bounds.lower} and {bounds.upper} {bounds.units}"
+        return label, description
+
+    def chronicHeat_label_description(self, bounds: HazardIndicatorBounds):
+        label = f"Max days per year between {bounds.lower} and {bounds.upper} {bounds.units}"
+        description = f"Max days per year between {bounds.lower} and {bounds.upper} {bounds.units}"
         return label, description
 
     def calc_measure(
@@ -238,4 +262,4 @@ class GenericScoreBasedRiskMeasures(RiskMeasureCalculator):
         return self._definition_lookup.get(hazard_type, None)
 
     def supported_hazards(self) -> Set[type]:
-        return set([Wind])  # RiverineInundation, CoastalInundation,
+        return set([Wind, Hail, Drought, Fire, Precipitation, ChronicHeat])  # RiverineInundation, CoastalInundation,

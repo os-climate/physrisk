Tidying and test improvements; fix precision-related bug in applying SOP.
Signed-off-by: Joe Moorhouse <5102656+joemoorhouse@users.noreply.github.com>

src/physrisk/data/pregenerated_hazard_model.py
@@ -113,7 +113,7 @@ class PregeneratedHazardModel(HazardModel):
                             np.array([0]),
                         )
                     responses[req] = HazardEventDataResponse(
-                        valid_periods, valid_intensities, units, path
+                        valid_periods, valid_intensities.astype(dtype="float64"), units, path
                     )
             else:  # type: ignore
                 parameters, defns, units, path = hazard_data_provider.get_data(
@@ -128,7 +128,7 @@ class PregeneratedHazardModel(HazardModel):
                 for i, req in enumerate(batch):
                     valid = ~np.isnan(parameters[i, :])
                     responses[req] = HazardParameterDataResponse(
-                        parameters[i, :][valid], defns[valid], units, path
+                        parameters[i, :][valid].astype(dtype="float64"), defns[valid], units, path
                     )
         except Exception as err:
             # e.g. the requested data is unavailable

src/physrisk/kernel/curve.py
@@ -18,7 +18,7 @@ def add_x_value_to_curve(x, curve_x, curve_y):
         curve_x = np.insert(curve_x, i, x)
     elif x == curve_x[i]:
         # point already exists; nothing to do
-        return curve_x, curve_y
+        return curve_x, curve_y, i
     elif i == 0:
         curve_y = np.insert(curve_y, 0, curve_y[0])  # flat extrapolation
         curve_x = np.insert(curve_x, 0, x)
@@ -30,7 +30,7 @@ def add_x_value_to_curve(x, curve_x, curve_y):
         curve_y = np.insert(curve_y, i, prob)
         curve_x = np.insert(curve_x, i, x)
 
-    return curve_x, curve_y
+    return curve_x, curve_y, i
 
 
 def to_exceedance_curve(bin_edges, probs):
@@ -148,7 +148,7 @@ class ExceedanceCurve:
         """Add a point to the curve with specified value and exceedance
         probability determined from existing curve by linear interpolation.
         """
-        values, probs = add_x_value_to_curve(value, self.values, self.probs)
+        values, probs, _ = add_x_value_to_curve(value, self.values, self.probs)
         return ExceedanceCurve(probs, values)
 
     def get_value(self, prob):
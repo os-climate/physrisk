Add visualisation for JBA commercial model and fix units propagation. (#589)
* Add visualisation for JBA commerical model and fix units propagation.

Signed-off-by: joemoorhouse <joe.moorhouse@gmail.com>

* Chore: pre-commit.ci automated linting tools update

Signed-off-by: pre-commit-ci[bot] <pre-commit-ci[bot]@pre-commit.ci>

Changes in this pull request are as follows:

---------

Signed-off-by: joemoorhouse <joe.moorhouse@gmail.com>
Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

src/physrisk/data/hazard_data_provider.py
@@ -56,12 +56,15 @@ class ResourcePaths:
     """Provides for a single HazardResource the mappings of scenarios to the ScenarioPaths.
     The ScenarioPaths for each scenario will give the available years and a function to get
     the array path for a year.
+    Units (inferred from the inventory) are also provided as these are needed in addition
+    to path information to derive HazardDataResponses.
     """
 
     # the path (unique identifier) of the HazardResource
     resource_path: str
     # note the key is the *requested* scenario, not the scenario it may be proxied to
     scenarios: Dict[str, ScenarioPaths]
+    units: str
 
 
 class SourcePaths(Protocol):
@@ -314,6 +317,7 @@ class HazardDataProvider(ABC):
         result: Dict[ScenarioYear, ScenarioYearResult] = {}
         # Retrieve the data for all available years for the path in question.
         weights: Dict[ScenarioYear, WeightedSum] = {}
+        expected_units = resource_paths.units
         for scenario, paths in resource_paths.scenarios.items():
             if len(paths.years) == 0:
                 continue
@@ -360,7 +364,7 @@ class HazardDataProvider(ABC):
                         [len(indices)], dtype=np.int32
                     ),  # a numpy scalar
                     coverage_mask=coverage,
-                    units=units,
+                    units=expected_units if units == "default" else units,
                     paths=np.array(
                         [sys.intern(resource_paths.resource_path)], dtype=np.object_
                     ),

src/physrisk/hazard_models/jba_hazard_model.py
@@ -616,10 +616,13 @@ class JBAHazardModel(HazardModel):
     def _process_response(self, request: HazardDataRequest, response: Dict):
         if request.hazard_type == RiverineInundation:
             tag = "FLRF_U"
+            path = "jba_riverine"
         elif request.hazard_type == PluvialInundation:
             tag = "FLSW_U"
+            path = "jba_pluvial"
         elif request.hazard_type == CoastalInundation:
             tag = "STSU_U"
+            path = "jba_coastal"
         else:
             raise ValueError("unexpected hazard type")
         if response["stats"] is None:
@@ -627,7 +630,7 @@ class JBAHazardModel(HazardModel):
         elif request.indicator_id == "flood_sop":
             sop = response["stats"].get("FLRF_U", {}).get("sop", 0)
             return HazardParameterDataResponse(
-                [sop, sop], units="years", path="jba"
+                [sop, sop], units="years", path=path
             )  # min and max: in this case just a single value
         elif request.indicator_id == "flood_depth":
             return_periods: List[float] = []
@@ -641,7 +644,7 @@ class JBAHazardModel(HazardModel):
                 np.array(return_periods),
                 np.array(intens),
                 units="m",
-                path="jba",
+                path=path,
             )
         else:
             raise NotImplementedError()